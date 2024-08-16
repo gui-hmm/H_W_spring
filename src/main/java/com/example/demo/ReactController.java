7a868f8b4297f0488d173360a0e1856d5f4e88f3
Guilherme Henrique <henrquemelomoura@gmail.com>
2024-08-16 10:34:09 -0300

    first commit

---
+++ src/main/java/com/example/demo/ReactController.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ReactController {
+
+    @RequestMapping(value = "/{path:[^\\.]*}")
+    public String redirect() {
+        // Redireciona todas as rotas que não sejam para arquivos estáticos para o index.html
+        return "forward:/index.html";
+    }
+}
